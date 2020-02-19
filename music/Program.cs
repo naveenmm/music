8691d48b1fe025d9236a5e379713543469ccb053
naveenmm <naveen.mm@soti.net>
2020-02-19 17:12:37 +0530

    Changes

--- music/Program.cs
+++ music/Program.cs
@@ -8,14 +8,16 @@ namespace music
         {
             int[] major = new int[] { 2,2,1,2,2,2,1 };
             int[] minor = new int[] { 2, 1, 2, 2, 2, 2, 1 };
-            Console.WriteLine("Enter Scale \n 1.Major \n 2.Minor");
+            int[] Pentatonic_Minor = new int[] { 3, 2, 2, 3, 2 };
+            int[] chromatic = new int[] { 1,1,1,1,1,1,1,1,1,1,1,1,1 };
+            Console.WriteLine("Enter Scale \n 1.Major \n 2.Minor \n3.Pentatonic Minor Scale \n4.Chromatic");
             string scale=Console.ReadLine();
             string[] b= { "C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
             Console.WriteLine("Enter value:");
             string val = Console.ReadLine();
             int pos=0;
             int i,j;
-            int[] s = new int[7];
+            int[] s = new int[13];
             for (i = 0; i < 12; i++)
             {
                 if (val == b[i])
@@ -24,27 +26,42 @@ namespace music
                 }
             }
             Console.Write(b[pos]+" ");
-           
 
             if (scale == "1")
             {
                 for (int k = 0; k < 7; k++)
                 {
                     s[k] = major[k];
-                    
+
                 }
             }
-            else 
+            else if (scale == "2")
             {
                 for (int k = 0; k < 7; k++)
                 {
                     s[k] = minor[k];
-                   
+
+                }
+            }
+            else if (scale == "3")
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    s[k] = Pentatonic_Minor[k];
+
+                }
+            }
+            else 
+            {
+                for (int k = 0; k < 13; k++)
+                {
+                    s[k] = chromatic[k];
+
                 }
             }
 
-           for (j = 0; j < 7; j++)
-           {
+            for (j = 0; j < 13; j++)
+            {
                 int s1 = s[j];
                 Console.Write(b[(pos + s[j]) % 12] +" ");
                 pos = pos + s1;
