72482f28d0caa6fdbd21f1f47b96f73ed0f5cc99
naveenmm <naveen.mm@soti.net>
2020-02-22 01:16:50 +0530

    Added scales data in scales.json file. Added new class to get sequences and display.

--- music/Program.cs
+++ music/Program.cs
@@ -6,66 +6,23 @@ namespace music
     {
         static void Main(string[] args)
         {
-            int[] major = new int[] { 2,2,1,2,2,2,1 };
-            int[] minor = new int[] { 2, 1, 2, 2, 2, 2, 1 };
-            int[] Pentatonic_Minor = new int[] { 3, 2, 2, 3, 2 };
-            int[] chromatic = new int[] { 1,1,1,1,1,1,1,1,1,1,1,1,1 };
-            Console.WriteLine("Enter Scale \n 1.Major \n 2.Minor \n3.Pentatonic Minor Scale \n4.Chromatic");
-            string scale=Console.ReadLine();
-            string[] b= { "C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
-            Console.WriteLine("Enter value:");
+            Console.WriteLine("Enter Scale name::");
+            var scale = new Scales();
+            string name=Console.ReadLine();
+            scale.name = name;
+            double sequence=scale.getsequence(name);
+            Console.WriteLine(sequence);
+            int[] s = scale.note(sequence);
+            //for(int i=0;i<s.Length;i++)
+            //{
+            //    Console.WriteLine(s[i]);
+            //}                                        
+            //string[] b= { "C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+            Console.WriteLine("Enter start value:");
             string val = Console.ReadLine();
-            int pos=0;
-            int i,j;
-            int[] s = new int[13];
-            for (i = 0; i < 12; i++)
-            {
-                if (val == b[i])
-                {
-                    pos = i;
-                }
-            }
-            Console.Write(b[pos]+" ");
-
-            if (scale == "1")
-            {
-                for (int k = 0; k < 7; k++)
-                {
-                    s[k] = major[k];
-
-                }
-            }
-            else if (scale == "2")
-            {
-                for (int k = 0; k < 7; k++)
-                {
-                    s[k] = minor[k];
-
-                }
-            }
-            else if (scale == "3")
-            {
-                for (int k = 0; k < 5; k++)
-                {
-                    s[k] = Pentatonic_Minor[k];
-
-                }
-            }
-            else 
-            {
-                for (int k = 0; k < 13; k++)
-                {
-                    s[k] = chromatic[k];
-
-                }
-            }
-
-            for (j = 0; j < 13; j++)
-            {
-                int s1 = s[j];
-                Console.Write(b[(pos + s[j]) % 12] +" ");
-                pos = pos + s1;
-            }       
+            scale.show(val,s);
+                        
+            
         }
     }
 }
---
+++ music/Scales.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace music
+{
+    public class Scales
+    {
+        public double sequence { get; set; }
+        public string name { get; set; }
+
+        public double getsequence(string n)
+        {
+            StreamReader r = new StreamReader("C:\\Users\\admin.INL083\\source\\repos\\music\\music\\scales.json");
+            string json = r.ReadToEnd();
+            List<Scales> items = JsonConvert.DeserializeObject<List<Scales>>(json);
+            dynamic array = JsonConvert.DeserializeObject(json);
+            //string seq;
+            foreach (var item in array)
+            {
+                if (item.name == n)
+                {
+                    sequence = item.sequence;
+                }                
+            }
+            if (sequence==0)
+            {
+                Console.WriteLine("Scale not available::");
+                foreach (var item in array)
+                {
+                    Console.WriteLine(item.name);
+                }
+                Console.WriteLine("Enter any of these::");
+                n=Console.ReadLine();
+                getsequence(n);
+            }
+            return sequence;
+            
+        }
+
+        public int[] note(double seq)
+        {
+            seq= Convert.ToInt32(seq);
+            string seq2 = seq.ToString();
+            int[] seq3 = new int[seq2.Length];
+            for(int i = 0; i<seq2.Length; i++)
+            {
+                seq3[i] = Convert.ToInt32(seq2[i]-48);                                
+            }
+            //int[] seq3 = seq2.Split('').Select(n => Convert.ToInt32(n)).ToArray();
+            //int[] outarry = Array.ConvertAll(seq.ToString().ToArray(), x => (int)x);
+            return seq3;
+        }
+
+        public string[] show(string val,int[] note)
+        {
+            string[] b = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+            int pos = 0;
+            string[] a = new string[note.Length];
+            for (int i = 0; i < 12; i++)
+            {
+                if (val == b[i])
+                {
+                    pos = i;
+                    for (int k = 0; k < note.Length; k++)
+                    {
+                        a[k] = b[pos%12];
+                        pos = pos + note[k];
+                        Console.Write(a[k]+" ");
+                    }
+                }
+            }
+            return a;                       
+        }
+    }
+}
